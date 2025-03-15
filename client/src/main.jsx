52075585615b11286d4f719ef1a0c7dacd2cced3
Danne <daniel.theoren@gmail.com>
2025-03-15 19:26:13 +0100

    Frontend WIP
    Routes för get och post skapade

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,37 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect, useState } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route} from "react-router";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <BrowserRouter>
+          <Routes>
+              <Route path="/kontaktlista" element={<FetchKontaktlista/>} />
+              <Route path="/add-kontakt" element={<PostKontakt/>} />
+          </Routes>
+      </BrowserRouter>
   </StrictMode>,
 )
+
+function FetchKontaktlista() {
+    const [kontakt, setKontakt] = useState([]);
+    
+    useEffect(() => {
+        fetch("/api/kontaktlista")
+            .then(response => response.json())
+            .then(data => setKontakt(data));
+        }
+    )
+}
+
+function PostKontakt(kontakt) {
+    const response = 
+        fetch("/api/nyKontakt", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                namn: kontakt.namn,
+                nummer: kontakt.nummer,     
+            })
+    })
+}
